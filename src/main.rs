46405da443384b27b4a3352d98fc9c150b0724e7
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2021-05-13 00:01:00 +0200

    Implement healthcheck endpoint 200 OK

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use std::io;
 
-async fn greet(request: HttpRequest) -> impl Responder {
-    let name = request.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+async fn health() -> impl Responder {
+    HttpResponse::Ok()
 }
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/health", web::get().to(health)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
